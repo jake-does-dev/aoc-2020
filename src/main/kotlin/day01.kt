618cfe2d69f148fdb5fa1289ff5d9d5df65e6a5d
Jake Allan <jjemail1995@gmail.com>
2022-11-06 15:20:10 +0000

    aoc2020 day01

---
+++ src/main/kotlin/day01.kt
@@ -0,0 +1,40 @@
+import java.io.File
+
+fun multiplyPair(sum: Int, file: File) : Int? =
+    file.readLines()
+        .map(String::toInt)
+        .multiplyPairThatSumsTo(sum)
+
+fun multiplyTriple(sum: Int, file: File) : Int? =
+    file.readLines()
+        .map(String::toInt)
+        .multiplyTripleThatSumsTo(sum)
+
+private fun List<Int>.multiplyPairThatSumsTo(sum: Int): Int? =
+    findPairThatSumsTo(sum)?.multiply()
+
+private fun List<Int>.multiplyTripleThatSumsTo(sum: Int): Int? =
+    findTripleThatSumsTo(sum)?.multiply()
+
+private fun List<Int>.findPairThatSumsTo(sum: Int): Pair<Int, Int>? {
+    val complements = associateBy { sum - it }
+    val pair = firstNotNullOfOrNull { number ->
+        val complement = complements[number]
+        if (complement != null) {
+            Pair(number, complement)
+        } else null
+    }
+    return pair
+}
+
+private fun List<Int>.findTripleThatSumsTo(sum: Int): Triple<Int, Int, Int>? =
+    firstNotNullOfOrNull { number ->
+        val pair = findPairThatSumsTo(sum - number)
+        if (pair != null) return Triple(number, pair.first, pair.second) else null
+    }
+
+private fun Pair<Int, Int>.multiply() : Int =
+    first * second
+
+private fun Triple<Int, Int, Int>.multiply() : Int =
+    first * second * third
---
+++ src/test/kotlin/Day01KtTest.kt
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.io.File
+
+internal class Day01KtTest {
+
+    @Test
+    fun multiplyPairExampleInput() {
+        assertEquals(514579, multiplyPair(2020, File("src/main/resources/day01/example.txt")))
+    }
+
+    @Test
+    fun multiplyPairPuzzleInput() {
+        assertEquals(1007104, multiplyPair(2020, File("src/main/resources/day01/puzzle.txt")))
+    }
+
+    @Test
+    fun multiplyTripleExampleInput() {
+        assertEquals(241861950, multiplyTriple(2020, File("src/main/resources/day01/example.txt")))
+    }
+
+    @Test
+    fun multiplyTriplePuzzleInput() {
+        assertEquals(18847752, multiplyTriple(2020, File("src/main/resources/day01/puzzle.txt")))
+    }
+}
